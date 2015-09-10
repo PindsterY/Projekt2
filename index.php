538e6521bb2fd193157677f84a60eaeb9b27f916
Rasmus Pind <rasmus.pind@khk.ee>
2015-09-10 11:31:38 +0300

    Esimene comitt, rohkem kontrolliks!

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <title>Projekt 2</title>
+</head>
+
+<body>
+<!---- Siia võin hakata kirjutama sisu -->
+<h1>Heading 1</h1>
+<p>Tekst tuleb headingu alla</p>
+
+<h2>Heading 2</h2>
+<p>Tekst tuleb heading2 alla</p>
+
+<h3>Heading 3</h3>
+<p>Tekst tuleb heading3 alla</p>
+
+<h4>Heading 4</h4>
+<p>Tekst tuleb heading4 alla</p>
+
+<h5>Heading 5</h5>
+<p>Tekst tuleb heading5 alla</p>
+
+<h6>Heading 6</h6>
+<p>Tekst tuleb heading6 alla</p>
+
+<p>Et teha pikk teemat vahetav joon ekraanile, siis kasutan hr tagi</p>
+<hr>
+
+<p>Et täpsemalt teada saada tagide kohta, siis tuleks kasutada <a href="http://www.google.com">Google otsingumootor</a>'i abi</p>
+<p>Samas on otseloomulikult kõige parem ju minna <abbr title="World Wide Web Consortium">W3C</abbr> lehele. </p>
+
+</body>
+
+</html>
